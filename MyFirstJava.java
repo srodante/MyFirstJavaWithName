eb00ba264831e0d517eda7cd383228d53100d757
srodante <91100121+srodante@users.noreply.github.com>
2021-09-21 14:41:21 -0400

    Add files via upload

---
+++ MyFirstJava.java
@@ -0,0 +1,7 @@
+public class MyFirstJava {
+    public static void main(String args[]) {
+        System.out.println("Hello world.");
+        System.out.println("Today is September 21, 2021. I am alive!");
+        System.out.println("My name is Scott.");
+    }
+}
